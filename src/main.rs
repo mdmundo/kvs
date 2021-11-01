32f5661d767c95acedeb9375d920f5d12783ef1e
Edmundo Paulino <manyymoore@gmail.com>
2021-11-01 10:25:42 -0300

    refactor: 🎨 Improve structure
    Add cli option: add

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ mod db;
 
 fn main() {
     let mut args = std::env::args().skip(1);
-    // let action = args.next().expect("Action required");
+    let action = args.next().expect("Action required");
     // add, rm, get
-    let key = args.next().expect("Key required");
-    // expect: Panics if the value is a [None] with a custom panic message provided by msg.
-    let value = args.next().unwrap();
-    // unwrap: Panics if the self value equals [None].
-    // println!("The key is '{}' and the value is '{}'", key, value);
-    let mut database = db::Database::new().expect("Failed to create database");
-    database.insert(key.to_uppercase(), value.clone());
-    // to_uppercase: Returns a new String, so I'm able to use `key` it again.
-    // clone: Returns a copy of the value.
-    database.flush().unwrap();
+    if action.eq("add") {
+        let key = args.next().expect("Key required");
+        // expect: Panics if the value is a [None] with a custom panic message provided by msg.
+        let value = args.next().unwrap();
+        // unwrap: Panics if the self value equals [None].
+        let mut database = db::Database::new().expect("Failed to create database");
+        database.insert(key.to_uppercase(), value.clone());
+        // to_uppercase: Returns a new String, so I'm able to use `key` it again.
+        // clone: Returns a copy of the value.
+        database.flush().unwrap();
+    }
 }
