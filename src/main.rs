88195f753e00d81cdedef902a95b87aaed986b76
Edmundo Paulino <manyymoore@gmail.com>
2021-11-01 10:52:48 -0300

    refactor: ⚰️ Remove comments

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod db;
 fn main() {
     let mut args = std::env::args().skip(1);
     let action = args.next().expect("Action required");
-    // add, rm, get
     if action.eq("add") {
         let key = args.next().expect("Key required");
         // expect: Panics if the value is a [None] with a custom panic message provided by msg.
